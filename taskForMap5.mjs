13c1a499aef409f97a60daa55d4048964df83833
idaspa <idahelenhal@msn.com>
2025-02-17 16:47:04 +0100

    same code without left turn

---
+++ taskForMap5.mjs
@@ -0,0 +1,47 @@
+
+
+do {
+    move();
+
+    if (!peek()) {
+        turn();
+
+    }
+
+} while (!atGoal())
+
+
+
+
+function move() {
+    console.log("Move one tile foreward");
+
+}
+function turnLeft() {
+    turn()
+    turn()
+    turn()
+
+    console.log("left turn")
+}
+
+function turn() {
+   
+    console.log("Turned 90 degrees");
+}
+
+function peek() {
+
+    console.log("Checked if it's open spot in front of me");
+
+    return true;
+}
+
+function atGoal() {
+
+    console.log("Checked if I'm at Goal");
+
+    return true;
+}
+
+
